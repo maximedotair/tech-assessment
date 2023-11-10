2c222cee99c6997ac867266f2ce74f2d45cf24c1
Maximedotair <maxime.ramos75@gmail.com>
2023-11-10 16:48:05 +0100

    Update eligibility.service.js

--- src/eligibility.service.js
+++ src/eligibility.service.js
@@ -1,18 +1,40 @@
 class EligibilityService {
-  /**
-   * Compare cart data with criteria to compute eligibility.
-   * If all criteria are fulfilled then the cart is eligible (return true).
-   *
-   * @param cart
-   * @param criteria
-   * @return {boolean}
-   */
   isEligible(cart, criteria) {
-    // TODO: compute cart eligibility here.
-    return false;
+      const getValue = (obj, path) => {
+          return path.split('.').reduce((value, key) => {
+              if (value && typeof value === 'object') {
+                  return value[key];
+              }
+              return undefined;
+          }, obj);
+      };
+
+      const checkCondition = (cartValue, condition) => {
+          if (typeof condition !== 'object') {
+              return cartValue == condition; 
+          }
+
+          if (condition.gt !== undefined) return cartValue > condition.gt;
+          if (condition.lt !== undefined) return cartValue < condition.lt;
+          if (condition.gte !== undefined) return cartValue >= condition.gte;
+          if (condition.lte !== undefined) return cartValue <= condition.lte;
+          if (condition.in !== undefined) return condition.in.includes(cartValue);
+          if (condition.and !== undefined) return condition.and.every(subCond => checkCondition(cartValue, subCond));
+          if (condition.or !== undefined) return condition.or.some(subCond => checkCondition(cartValue, subCond));
+
+          return false; 
+      };
+
+      for (const key in criteria) {
+          if (criteria.hasOwnProperty(key)) {
+              const cartValue = getValue(cart, key);
+              if (!checkCondition(cartValue, criteria[key])) return false;
+          }
+      }
+      return true;
   }
 }
 
 module.exports = {
-  EligibilityService,
+EligibilityService,
 };
